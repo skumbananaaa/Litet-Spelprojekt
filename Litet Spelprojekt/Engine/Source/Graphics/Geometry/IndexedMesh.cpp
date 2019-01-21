586084606ba76b6c12c8e4490ee3e1f466db3634
Mumsfilibaba <alexander@dahlins.nu>
2019-01-21 17:44:16 +0100

    Fixed Hermans bug

--- Litet Spelprojekt/Engine/Source/Graphics/Geometry/IndexedMesh.cpp
+++ Litet Spelprojekt/Engine/Source/Graphics/Geometry/IndexedMesh.cpp
@@ -13,10 +13,10 @@ IndexedMesh::IndexedMesh(const Vertex* const vertices, const unsigned int* const
 	glBindVertexArray(m_VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-	glBufferData(GL_ARRAY_BUFFER, m_VertexCount, vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, m_VertexCount * sizeof(Vertex), vertices, GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_IndexCount, indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_IndexCount * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0); //Position
 	glEnableVertexAttribArray(0);

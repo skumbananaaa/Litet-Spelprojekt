502670f044a14ec8c3e0ebcf5a1dded85fff6b7f
skumbananaaa <skumbananaaa@gmail.com>
2019-01-21 13:46:06 +0100

    Application

--- Litet Spelprojekt/Engine/Source/System/Application.cpp
+++ Litet Spelprojekt/Engine/Source/System/Application.cpp
@@ -1,10 +1,15 @@
 #include <EnginePch.h>
+#include <chrono>
 #include <System/Application.h>
+
+using namespace std::chrono_literals;
+constexpr std::chrono::nanoseconds timestep(16ms);
+constexpr float timestepCount = timestep.count();
 
 Application* Application::s_Instance = nullptr;
 
 Application::Application()
-	: m_pWindow(nullptr)
+	: m_pWindow(nullptr), m_pContext(nullptr)
 {
 	assert(s_Instance == nullptr);
 	s_Instance = this;
@@ -17,6 +22,7 @@ Application::Application()
 	else
 	{
 		m_pWindow = new Window(1024, 768);
+		m_pContext = new GLRenderer();
 	}
 	
 
@@ -34,9 +40,46 @@ Application::~Application()
 		m_pWindow = nullptr;
 	}
 
+	if (m_pRenderer != nullptr)
+	{
+		delete m_pRenderer;
+		m_pRenderer = nullptr;
+	}
+
 	glfwTerminate();
 
 	std::cout << "Nudes deleted" << std::endl;
+}
+
+int32_t Application::Run()
+{
+	using clock = std::chrono::high_resolution_clock;
+	using timepoint = std::chrono::time_point<std::chrono::steady_clock>;
+	using duration = std::chrono::duration<float>;
+
+	timepoint currentTime;
+	timepoint prevTime;
+	duration deltaTime;
+	duration accumulator(0ns);
+
+	while (!m_pWindow->isClosed())
+	{
+		currentTime = clock::now();
+		deltaTime = currentTime - prevTime;
+		prevTime = currentTime;
+
+		accumulator += deltaTime;
+
+		while (accumulator > timestep)
+		{
+			OnUpdate(timestepCount);
+			accumulator -= timestep;
+		}
+
+		OnRender();
+	}
+
+	return 0;
 }
 
 Application& Application::GetInstance()

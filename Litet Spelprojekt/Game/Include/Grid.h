65ef41959089ba3c96b519b24ffdcc5885ee787a
Schtekt <totimom@live.se>
2019-01-29 14:13:22 +0100

    Added Tile and Grid

--- Litet Spelprojekt/Game/Include/Grid.h
+++ Litet Spelprojekt/Game/Include/Grid.h
@@ -1,20 +1,22 @@
 #ifndef GRID_H
 #define GRID_H
-#include <GLM\vec2.hpp>
-#include <Graphics/Geometry/Mesh.h>
+#include "Tile.h"
 class Grid
 {
 private:
-	glm::ivec2 m_size;
-	int ** m_ppTiles;
-	Mesh * m_pMesh;
+	glm::ivec2 m_Size;
+	glm::vec2 m_Pos;
+	Tile *** m_pppTiles;
 public:
-	Grid(const glm::ivec2 & size, const int ** tiles);
-	Grid(const glm::ivec2 & size = glm::ivec2(0, 0));
+	Grid(const glm::ivec2 & size, const unsigned int ** tileVal, const glm::vec3 position = glm::vec3(0.0f));
+	Grid(const glm::ivec2 & size = glm::ivec2(0, 0), const glm::vec3 position = glm::vec3(0.0f));
 	~Grid();
 
-	void edit(const glm::ivec2 & pos, const int & val);
+	void edit(const glm::ivec2 & pos, const unsigned int & val);
 	const int getVal(const glm::ivec2 & pos) const;
-	void onRender(); //????????
+	Tile * GetTile(const glm::ivec2 & index);
+	const glm::ivec2 GetSize() const;
+
+	void SetColor(const glm::ivec2 & pos, const glm::vec4 & color);
 };
 #endif

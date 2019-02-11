4efa9a64765bb33221a6ddc295819fceaa0550df
Herman Hansson Söderlund <skumbananaaa@gmail.com>
2019-02-11 15:19:07 +0100

    Fix Water Light Bug Again  Because of Git

--- Litet Spelprojekt/Game/Resources/Shaders/waterVert.glsl
+++ Litet Spelprojekt/Game/Resources/Shaders/waterVert.glsl
@@ -21,6 +21,8 @@ layout(std140, binding = 0) uniform CameraBuffer
 	mat4 g_Projection;
 	mat4 g_InverseView;
 	mat4 g_InverseProjection;
+	vec3 g_CameraLookAt;
+	float pad1;
 	vec3 g_CameraPosition;
 };
 

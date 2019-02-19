36f5a7f6cdba97aea763748bfe0a961f5c615a45
Herman Hansson Söderlund <skumbananaaa@gmail.com>
2019-02-19 15:43:49 +0100

    Merge Fix

--- Litet Spelprojekt/Engine/Include/World/Scenarios/Fire/FireAlarm.h
+++ Litet Spelprojekt/Engine/Include/World/Scenarios/Fire/FireAlarm.h
@@ -3,14 +3,21 @@
 #include <Audio/Sources/AudioSource.h>
 #include <Graphics/GameObject.h>
 
+class SpotLight;
+
 class API FireAlarm : public GameObject
 {
 public:
 	FireAlarm(int32 source);
-	void SetPosition(const glm::vec3& position) noexcept;
-	void SetDetection(bool fireDetected);
-	void Update(float dt) noexcept;
+	virtual void SetPosition(const glm::vec3& position) noexcept override;
+	void TurnOff() noexcept;
+	virtual void Update(float dt) noexcept override;
+	virtual void OnSmokeDetected() noexcept override;
+	virtual void OnAddedToScene(Scene* scene) noexcept override;
+
 private:
 	bool m_FireDetected;
+	float m_Rotation;
 	AudioSource* m_Src;
+	SpotLight* m_Spotlight;
 };

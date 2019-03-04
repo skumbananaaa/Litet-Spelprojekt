f0342d9e742c32e1e521a9575f0a6b94530f02b5
Tim Johansson <totimom@live.se>
2019-03-04 13:57:47 +0100

    Added an order to aid fellow crewmember

---
+++ Litet Spelprojekt/Game/Source/Orders/OrderGiveAid.cpp
@@ -0,0 +1,60 @@
+#include "../../Include/Orders/OrderGiveAid.h"
+#include "../../Include/Crewmember.h"
+OrderGiveAid::OrderGiveAid(Crewmember* injuredMember): OrderWalk(injuredMember->GetTile())
+{
+	m_pAiding = injuredMember;
+	m_Target = injuredMember->GetTile();
+}
+
+OrderGiveAid::~OrderGiveAid()
+{
+}
+
+void OrderGiveAid::StartOrder(Scene* pScene, World* pWorld, Crew* pCrewMembers) noexcept
+{
+	OrderWalk::StartOrder(pScene, pWorld, pCrewMembers);
+}
+
+bool OrderGiveAid::UpdateOrder(Scene * pScene, World * pWorld, Crew * pCrewMembers, float dtS) noexcept
+{
+	bool res = OrderWalk::UpdateOrder(pScene, pWorld, pCrewMembers, dtS);
+	
+	if (res)
+	{
+		res = m_pAiding->Heal(GetCrewMember()->GetSkillMedic());
+	}
+	return res;
+}
+
+void OrderGiveAid::EndOrder(Scene * pScene, World * pWorld, Crew * pCrewMembers) noexcept
+{
+	OrderWalk::EndOrder(pScene, pWorld, pCrewMembers);
+
+	m_pAiding = nullptr;
+}
+
+void OrderGiveAid::AbortOrder(Scene * pScene, World * pWorld, Crew * pCrewMembers) noexcept
+{
+	OrderWalk::AbortOrder(pScene, pWorld, pCrewMembers);
+	m_pAiding = nullptr;
+}
+
+bool OrderGiveAid::AllowsMultipleOrders() noexcept
+{
+	return false;
+}
+
+std::string OrderGiveAid::GetName() noexcept
+{
+	return "Give Aid";
+}
+
+bool OrderGiveAid::ReadyToAbort() noexcept
+{
+	return OrderWalk::ReadyToAbort();
+}
+
+void OrderGiveAid::RunParallel()
+{
+	OrderWalk::RunParallel();
+}

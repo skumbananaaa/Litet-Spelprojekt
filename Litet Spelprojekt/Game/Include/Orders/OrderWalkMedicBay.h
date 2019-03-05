f5bd1bf1561c5ad0ce88451b6022994af48343ce
Tim Johansson <totimom@live.se>
2019-03-05 11:50:52 +0100

    Added order to walk to Medbay

---
+++ Litet Spelprojekt/Game/Include/Orders/OrderWalkMedicBay.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "OrderWalk.h"
+
+class OrderWalkMedicBay : public OrderWalk
+{
+public:
+	OrderWalkMedicBay(World* pWorld);
+	virtual ~OrderWalkMedicBay();
+
+	virtual void StartOrder(Scene* pScene, World* pWorld, Crew* pCrewMembers) noexcept override;
+/*	virtual bool UpdateOrder(Scene* pScene, World* pWorld, Crew* pCrewMembers, float dtS) noexcept override;
+	virtual void EndOrder(Scene* pScene, World* pWorld, Crew* pCrewMembers) noexcept override;
+	virtual void AbortOrder(Scene* pScene, World* pWorld, Crew* pCrewMembers) noexcept override;
+	virtual bool AllowsMultipleOrders() noexcept override;*/
+	virtual std::string GetName() noexcept override;
+	/*virtual bool ReadyToAbort() noexcept override;
+	virtual void RunParallel() override;*/
+};
